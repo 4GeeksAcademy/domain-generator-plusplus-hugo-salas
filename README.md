d6399ff0473963f0fbd0eae5a290fbbcf92b6335
HSalasPedr <119406196+HSalasPedr@users.noreply.github.com>
2025-11-03 10:44:50 +0000

    good practices applied

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # domain-generator-plusplus-hugo-salas
+
+4Geeks domain generator exercise with card visualization in HTML
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  $ pip3 install flask && python3 server.py
+```
---
+++ main.js
@@ -0,0 +1,59 @@
+// Domain parts
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon', 'internet', 'camiones', 'magnet'];
+let domain = ['com', 'es', 'dev', 'io', 'net'];
+
+// generate the domains
+let displayObj = {
+  com: [],
+  es: [],
+  dev: [],
+  io: [],
+  net: []
+}
+
+pronoun.forEach(domainPronoun => {
+  adj.forEach(domainAdjective => {
+    noun.forEach(domainNoun => {
+      domain.forEach(domainDoamins => {
+        if (domainNoun.endsWith(domainDoamins)) {
+          // If the last words in the noun in the domain matches the domain,
+          //  split the noun and end it with the domain directly
+          displayObj[domainDoamins].push(`${domainPronoun}${domainAdjective}\
+            ${domainNoun.slice(0, -domainDoamins.length)}.${domainDoamins}`)
+        } else {
+          displayObj[domainDoamins].push(`${domainPronoun}${domainAdjective}\
+          ${domainNoun}.${domainDoamins}`)
+        };
+      });
+    });
+  });
+});
+
+
+// Show on page
+function showCards() {
+  const container = document.getElementById("card-container");
+
+  for (const [key, value] of Object.entries(displayObj)) {
+    // Cards
+    const cardHTML = `
+        <div class="col-sm-12 col-md-4 mb-3 mx-auto">
+            <div class="card shadow-sm">
+                <div class="card-body text-center">
+                  <h5 class="card-title">${key}</h5>
+                  <p class="card-text">
+                    ${value.map(v => `<div>${v}</div>`).join("")}
+                  </p>
+                </div>
+            </div>
+        </div>
+    `;
+
+    container.innerHTML += cardHTML;
+  }
+}
+
+// Ni idea de qué es esto pero un señor de Stack overflow lo hizo y yo también y funciona
+document.addEventListener("DOMContentLoaded", showCards);
